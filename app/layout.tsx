60cf0abb8c00b5ebc7cab4ea062c516a4c6c24e0
Sh4dowyy <shadowyy1337@gmail.com>
2025-03-02 21:26:50 +0200

    thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <meta property="og:image" content="https://scontent-arn2-1.xx.fbcdn.net/v/t1.15752-9/477072929_625471866734505_5231902385237446960_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=0024fc&_nc_ohc=c5Fuvu0k4IUQ7kNvgHRzZPB&_nc_oc=AdgVYIySm7nT6LRw_SyXTlx2HNaXGmV0dxWwkjUTd7O4uDQRAz6oe9RUnT1HuJS1SKk&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-arn2-1.xx&oh=03_Q7cD1gEQwJA6pRolCkhoY0mHLEJIVJZ8IE-dtt6IM9daSrGgxg&oe=67EC0774" />
+      </head>
       <body className={`${inter.variable} ${playfair.variable} font-sans`}>{children}</body>
     </html>
   )
